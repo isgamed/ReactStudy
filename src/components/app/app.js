d9ece97b9b025c80c46cab055c5d105aac0a4cf0
Isgame <glass777@mail.ru>
2021-09-27 16:44:20 +0300

    Styled components

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,7 +7,12 @@ import PostList from '../post-list';
 import PostAddForm from '../post-add-form';
 
 import './app.css';
+import styled from 'styled-components';
 
+const AppBlock = styled.div`
+    margin: 0 auto;
+    max-width: 800px;
+`
 
 const App = () => {
 
@@ -18,15 +23,17 @@ const App = () => {
     ];
 
     return (
-        <div className="app">
+        <AppBlock>
             <AppHeader/>
             <div className="search-panel d-flex">
                 <SearchPanel/>
                 <PostStatusFilter/>
             </div>
-            <PostList posts={data}/>
+            <PostList 
+                posts={data}
+                onDelete={ id => console.log(id)}/>
             <PostAddForm/>
-        </div>
+        </AppBlock>
     )
 }
 
--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -1,25 +1,25 @@
 import React from "react";
+import { ListGroup } from 'reactstrap';
 
 import PostListItem from '../post-list-item';
 
-import './post-list.css';
-
-const PostList = ({posts}) => {
+const PostList = ({posts, onDelete}) => {
 
     const elements = posts.map((item) => {
         return (
             <li key={item.id} className='list-group-item'>
                 <PostListItem 
                 label={item.label} 
-                important={item.important}/>
+                important={item.important}
+                onDelete={() => onDelete(item.id)}/>
             </li>
         )
     });
 
     return (
-        <ul className="app-list list-group">
+        <ListGroup className="app-list">
             {elements}
-        </ul>
+        </ListGroup>
     )
 }
 
